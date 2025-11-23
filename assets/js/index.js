e722296e5815028320441bd05cd83edca01725f9
123usef <yousifm836@gmail>
2025-11-23 09:54:06 +0400

    initial Dom Code Structure

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+// console.log(document.getElementsByTagName("h2")[2].textContent);
+// let h2s = document.getElementsByTagName("h2");
+// console.log(h2s);
+// h2s[0].innerHTML = "3nb";
+// let head2 = document.getElementById("head-2");
+// console.log(head2);
+
+
+// let head22 = document.getElementsByClassName("head-22");
+// console.log(head22);
+// head22[0].innerHTML = "bl7";
+
+let query = document.querySelector("h2");
+let query1 = document.querySelector(".head-22");
+let query2 = document.querySelector("#head-2");
+let query3 = document.querySelectorAll(".head-22");
+let query4 = document.querySelectorAll("#head-2");
+
+console.log(query);
+console.log(query1);
+console.log(query2);
+console.log(query3);
+console.log(query4);
+
+// let head22 = document.getElementsByClassName("head-22");
+// console.log(head22);
+
+// let head22 = document.getElementsByClassName("head-22")[0];
+// console.log(head22);
+
+// let head22 = document.getElementsByClassName("head-22");
+// console.log(head22);
